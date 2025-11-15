a54c6fc85190b6664fe59d42a330c1a2d02f8a3d
Kevin Mongey <kevin.r.mongey@gmail.com>
2025-11-15 18:18:25 +0000

    Add project files.

---
+++ TimerApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TimerApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TimerApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TimerApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TimerApp.Pages")]
