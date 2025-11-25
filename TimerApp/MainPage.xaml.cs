d46f9f1d357246ade0b048bc242873eb7a7ef59d
Kevin Mongey <kevin.r.mongey@gmail.com>
2025-11-25 10:43:10 +0000

    Added basic timer code and removed maui template code

--- TimerApp/MainPage.xaml.cs
+++ TimerApp/MainPage.xaml.cs
@@ -1,24 +1,44 @@
-﻿namespace TimerApp
+﻿using Microsoft.Maui.Dispatching;
+
+namespace TimerApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private IDispatcherTimer _timer;
+        int _totalSeconds = 0;
+        bool _longBreak;
+        bool _shortBreak;
 
         public MainPage()
         {
             InitializeComponent();
+
+            _timer = Dispatcher.CreateTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += OnTimerTick;
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void StartTimer(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            //set seconds for 25 min timer
+            _totalSeconds = 25 * 60; 
+            _timer.Start();
         }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            _totalSeconds--;
+            if (_totalSeconds == 0)
+                _timer.Stop();
+
+            //seperate to mins and seconds, convert to string to pad with 0's
+            string secs = (_totalSeconds % 60).ToString();
+            string mins = (_totalSeconds / 60).ToString();
+            //secs.PadLeft(3, '0');
+            //mins.PadLeft(3, '0');
+
+            TimerLabel.Text = $"{mins}:{secs}";
+        }
+        
     }
 }
